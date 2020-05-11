332b0a2894e99758dc89b7b728283df07bf68698
Jess Enright <jenright@gmail.com>
2020-05-11 08:41:10 +0100

    Significant changes to implement use of real-style sample data, and fix bugs discovered while doing so.

--- sampleUseOfModel.py
+++ sampleUseOfModel.py
@@ -23,7 +23,7 @@ baseWeight = 0.5
 # strongWeight = 0.8
 ages=['y', 'm', 'o']
 numInfected = 10
-genericInfection = 0.4
+genericInfection = 0.1
 ageInfectionMatrix = {}
 contactRate = 0.2
 for age in ages:
@@ -45,55 +45,94 @@ baseGraph = nx.path_graph(10)
 for (u, v) in list(baseGraph.edges()):
      baseGraph[u][v]['weight'] = baseWeight
 #     
-states = ss.setupInternalPopulations(baseGraph, compNames, ages)
+
 # for node in states[0]:
 #     print(states[0][node])
 
 params = ss.readParametersAgeStructured(sys.argv[1])
-for guy in params:
-    print (params[guy])
+# for guy in params:
+#     print (params[guy])
 
 ageToTrans = ss.setUpParametersAges(params)
-for age in ageToTrans:
-    print (age)
-    print( ageToTrans[age])
+# for age in ageToTrans:
+#     print (age)
+#     print( ageToTrans[age])
+
+
+dictOfPops = ss.readPopulationAgeStructured(sys.argv[2])
+# for guy in dictOfPops:
+#     print("\n" + guy)
+#     print (dictOfPops[guy])
+#     
+# print("\n\n\n\n")
+graph = ss.genGraphFromContactFile(sys.argv[3])
+# # print (graph.edges())
+# # print (graph['S08000015']['S08000016'])
+
+
+# print("\n\n\n\n states examination")
+states = ss.setupInternalPopulations(graph, compNames, ages, ss.readPopulationAgeStructured(sys.argv[2]))
+print(states.keys())
+for node in states[0]:
+    print("\n\n" + str(node))
+    print (states[0][node])
 
 
 
-# pos = nx.get_node_attributes(baseGraph, 'pos')
-# strongEdges = generateChildcareEdgesAggregate(baseGraph, numGroups, sizeGroups)
-# for (u, v) in strongEdges:
-#     baseGraph.add_edge(u, v)
-#     baseGraph[u][v]['weight'] = strongWeight
-# 
-# baseGraph.add_edges_from(strongEdges)
-# 
-# longDistance = generateIllicitEdges(baseGraph, 100)
-# 
-# paraDict = readParameters(sys.argv[1])
-# fromStateTrans = setUpParametersVanilla(paraDict)
-# print(fromStateTrans)
-# 
+
+
+# # pos = nx.get_node_attributes(baseGraph, 'pos')
+# # strongEdges = generateChildcareEdgesAggregate(baseGraph, numGroups, sizeGroups)
+# # for (u, v) in strongEdges:
+# #     baseGraph.add_edge(u, v)
+# #     baseGraph[u][v]['weight'] = strongWeight
+# # 
+# # baseGraph.add_edges_from(strongEdges)
+# # 
+# # longDistance = generateIllicitEdges(baseGraph, 100)
+# # 
+# # paraDict = readParameters(sys.argv[1])
+# # fromStateTrans = setUpParametersVanilla(paraDict)
+# # print(fromStateTrans)
+# # 
 basicPlots = []
-# withGroups = []
-# withIllicit = []
-time = 20
+# # withGroups = []
+# # withIllicit = []
+time = 200
 numTrials = 1
-# for i in range(numTrials): 
-#     withGroups.append(basicSimulation(baseGraph, 4, time, 0.1))
-# print('Done withGroups')
+# # for i in range(numTrials): 
+# #     withGroups.append(basicSimulation(baseGraph, 4, time, 0.1))
+# # print('Done withGroups')
+
+print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
 for i in range(numTrials):
-     basicPlots.append(ss.basicSimulationInternalAgeStructure(baseGraph, numInfected, time, genericInfection, ageInfectionMatrix, ageToTrans))
-# # print('Done basic')
-# addIllicitEdges(baseGraph, sizeGroups^2*numGroups)
-# for i in range(numTrials):
-#     withIllicit.append(basicSimulation(baseGraph, 4, time, 0.1))
-# print('Done withIllicit')
-# 
-# plt.plot(generateMeanPlot(withIllicit), color='maroon', label = 'with_illicit')
-# plt.plot(generateMeanPlot(withGroups), color='green', label = 'with_groups')
+     basicPlots.append(ss.basicSimulationInternalAgeStructure(graph, numInfected, time, genericInfection, ageInfectionMatrix, ageToTrans, states))
+# # # print('Done basic')
+# # addIllicitEdges(baseGraph, sizeGroups^2*numGroups)
+# # for i in range(numTrials):
+# #     withIllicit.append(basicSimulation(baseGraph, 4, time, 0.1))
+# # print('Done withIllicit')
+# # 
+# # plt.plot(generateMeanPlot(withIllicit), color='maroon', label = 'with_illicit')
+# # plt.plot(generateMeanPlot(withGroups), color='green', label = 'with_groups')
 plt.plot(ss.generateMeanPlot(basicPlots), color = 'dodgerblue', label='basic')
-# plt.legend()
-# # plt.ylim(top=200)
-# 
+# # plt.legend()
+# # # plt.ylim(top=200)
+# # 
 plt.savefig('withAges.pdf')
+
+# 
+# 
+# debuggingState ={('y', 'S'): 100, ('m', 'S'): 100, ('o', 'S'): 100, ('y', 'E'): 100, ('m', 'E'): 100, ('o', 'E'): 100, ('y', 'A'): 100, ('m', 'A'): 100,
+#     ('o', 'A'): 100, ('y', 'I'): 100, ('m', 'I'): 100, ('o', 'I'): 100, ('y', 'H'): 100, ('m', 'H'): 100, ('o', 'H'): 100,
+#     ('y', 'R'): 100, ('m', 'R'): 100, ('o', 'R'): 100, ('y', 'D'): 100, ('m', 'D'): 100, ('o', 'D'): 100}
+# 
+# 
+# outState = ss.internalStateDiseaseUpdate(debuggingState, ageToTrans)
+# print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+# for key in outState:
+#     print(str(key) + str(outState[key]))
+
+
+
+
